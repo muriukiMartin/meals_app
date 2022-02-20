8936b8ac7257e35524a16d01c8b5a0bc6961b562
Martin <maritn.muriuki@strathmore.edu>
2022-02-20 05:32:05 +0300

    Register main screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,13 +7,18 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: dummyCategories.map((catData) => CategoryItem(catData.title, catData.color)).toList(),
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('Delicatessen'),),
+      body: GridView(
+        children: dummyCategories
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
       ),
     );
   }
