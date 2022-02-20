a11395a650f98db741e881f14bf663dea64c57fb
Martin <maritn.muriuki@strathmore.edu>
2022-02-20 10:20:18 +0300

    Create meal details screen and route

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import '../screens/meal_details_screen.dart';
 import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
   // const MealItem({ Key? key }) : super(key: key);
+  final String id;
   final String mealTitle;
   final String imageUrl;
   final int duration;
@@ -10,7 +12,8 @@ class MealItem extends StatelessWidget {
   final Affordability affordability;
 
   MealItem(
-      {@required this.mealTitle,
+      {@required this.id,
+      @required this.mealTitle,
       @required this.imageUrl,
       @required this.duration,
       @required this.complexity,
@@ -49,12 +52,17 @@ class MealItem extends StatelessWidget {
     }
   }
 
-  void selectMeal() {}
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      MealDetailScreen.routeName,
+      arguments: id,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: selectMeal,
+      onTap: () => selectMeal(context),
       child: Card(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(15),
