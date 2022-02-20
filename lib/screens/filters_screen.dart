47c4a2376b712202c23bc9b712ac1eb72c6a12ad
Martin <maritn.muriuki@strathmore.edu>
2022-02-20 12:39:14 +0300

    Use pushReplacementNamed() delete stackedscreens

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   // const FilterScreen({ Key? key }) : super(key: key);
@@ -6,8 +7,12 @@ class FiltersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters'),
+    return Scaffold(
+      appBar: AppBar(title: Text('Your Filters'),),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filters'),
+      ),
     );
   }
 }
